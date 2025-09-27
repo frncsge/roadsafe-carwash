7d0ed97570fe02554db3601fe696c2312cb202d8
Francis Ge <francisge527@gmail.com>
2025-09-27 23:19:58 +0800

    connect login form to express server

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-import express from 'express';
+import express from "express";
 import pg from "pg";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
+app.use(cors()); //i need to change this before deployment !!!
+app.use(express.json());
+
+app.post("/api/admin/login", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ trialOnly: "okay" });
+});
+
 app.listen(port, () => {
-    console.log("Server listening on port", port);
-})
+  console.log("Server listening on port", port);
+});
--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,15 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleInput(event) {
+    const inputType = event.target.id;
+    const newInputValue = event.target.value;
+    //check if input is from username or password.
+    if (inputType === "username") {
+      setUsername(newInputValue);
+    } else {
+      setPassword(newInputValue);
+    }
+  }
+
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/api/admin/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      //checks if server response is ok (200-299).
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Server response:", data);
+
+        //reset username and password states.
+        setUsername("");
+        setPassword("");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
   return (
     <div id="login-form-container">
-      <form id="login-form" action="/login" method="POST">
+      <form onSubmit={handleFormSubmit} id="login-form">
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" placeholder="Username" />
+        <input
+          onChange={handleInput}
+          id="username"
+          type="text"
+          placeholder="Username"
+          value={username}
+        />
         <label htmlFor="password">Password</label>
-        <input id="password" type="text" placeholder="Password" />
-        <a id="forgot-password" href="#" tabIndex={0}>Forgot password?</a>
-        <input id="login-button" type="submit" value="LOGIN" tabIndex={0}/>
+        <input
+          onChange={handleInput}
+          id="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+        />
+        <a id="forgot-password" href="#" tabIndex={0}>
+          Forgot password?
+        </a>
+        <input id="login-button" type="submit" value="LOGIN" tabIndex={0} />
       </form>
     </div>
   );
