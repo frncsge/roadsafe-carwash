1a2f388ad2a6d94a87c19310f3299cca5fcd7dbe
Francis Ge <francisge527@gmail.com>
2025-10-16 14:19:39 +0800

    add style to confirm delete modal

--- src/components/ConfirmDelete/ConfirmDelete.jsx
+++ src/components/ConfirmDelete/ConfirmDelete.jsx
@@ -1,24 +1,31 @@
-function ConfirmDelete({ deleteFrom, name, vehicle, confirmDelete }) {
-  const queueDeleteMessage = `You are about to delete ${vehicle} owned by ${name} from the queue list.`;
-  const staffDeleteMessage = `You are about to delete ${name} from the staff list.`;
+import { AiOutlineWarning } from "react-icons/ai";
 
+function ConfirmDelete({ deleteFrom, name, vehicle, confirmDelete }) {
   return (
     <div id="modal-backdrop" onClick={() => confirmDelete(false)}>
-      <div id="confirm-delete-modal">
-        <h1>CONFIRM DELETE</h1>
-        <p>
-          {deleteFrom === "queue"
-            ? queueDeleteMessage
-            : deleteFrom === "staff"
-            ? staffDeleteMessage
-            : null}
+      <div id="confirm-delete-modal" onClick={(e) => e.stopPropagation()}>
+        <AiOutlineWarning color="#db2b2b" size={60} />
+        <h1>Are you sure?</h1>
+        <p id="delete-modal-message">
+          {deleteFrom === "queue" && (
+            <>
+              You are about to delete <strong>{vehicle}</strong> owned by{" "}
+              <strong>{name}</strong> from the queue list.
+            </>
+          )}
+          {deleteFrom === "staff" && (
+            <>
+              You are about to delete <strong>{name}</strong> from the staff
+              list.
+            </>
+          )}
         </p>
-        <div id="modal-button-container">
-          <button className="modal-button" onClick={() => confirmDelete(true)}>
-            Proceed
+        <div id="delete-modal-button-container">
+          <button className="delete-modal-button" onClick={() => confirmDelete(false)}>
+            No, cancel
           </button>
-          <button className="modal-button" onClick={() => confirmDelete(false)}>
-            Discard
+          <button className="delete-modal-button" onClick={() => confirmDelete(true)}>
+            Yes, I'm sure
           </button>
         </div>
       </div>
